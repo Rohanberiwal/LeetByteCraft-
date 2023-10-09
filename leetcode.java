634b8962cd7505cae189dac6503f6ff2ece4ad50
ROHAN___BERIWAL <119040957+Rohanberiwal@users.noreply.github.com>
2023-10-09 17:03:19 +0530

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -3516,3 +3516,25 @@ class Solution {
     return false ;
     }
 } 
+// 1331 
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] nums = new int[arr.length];
+        for(int i = 0 ; i < nums.length; i ++){
+            nums[i] = arr[i];
+        }
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int j = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],j++);
+            }
+        }
+        for(int i = 0 ; i < nums.length; i ++){
+            nums[i] = map.get(arr[i]);
+        }
+        return nums;
+    }
+}
